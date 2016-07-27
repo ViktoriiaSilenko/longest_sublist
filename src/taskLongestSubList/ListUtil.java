739010dcd0b230a21bd5f7a37399d610e7629c54
1 <victory.silenko@gmail.com>
2016-07-28 01:04:22 +0300

    added comments

--- src/taskLongestSubList/ListUtil.java
+++ src/taskLongestSubList/ListUtil.java
@@ -6,16 +6,17 @@ import java.util.List;
 public class ListUtil {
 
 	/**
-	 * @param list
-	 *            is a list of pairs of numbers, e.g. [(1,4), (2,5), (7,
-	 *            3),(4,6),(7,7)
-	 * @return the longest sublist that has the pairs sorted by the first entry
-	 *         in ascending order by the second in descending order, e.g.
-	 *         [(2,5), (7, 3)]
+	 * @param list is a list of pairs of numbers, 
+	 * e.g. [(1,4), (2,5), (7, 3), (4, 6), (7, 7)
+	 * @return the longest sublist that has the pairs sorted by the first entry in ascending order by the second in descending order, 
+	 * e.g. [(2,5), (7, 3)]
 	 */
 	public static List<Pair> findLongestSubList(List<Pair> list) {
 
-		int tempCount = 1, count = 1, indexFromTemp = -1, indexFrom = -1;
+		int tempCount = 1;
+		int count = 1; // count of elements in longest sublist
+		int indexFromTemp = -1;
+		int indexFrom = -1; // first index in sublist
 
 		for (int i = 1; i < list.size(); i++) {
 			int sortByFirstNumber = new BigDecimal(list.get(i - 1).getFirstNumber().toString())
@@ -25,7 +26,7 @@ public class ListUtil {
 			if ((sortByFirstNumber == -1 || sortByFirstNumber == 0)
 					&& (sortBySecondNumber == 1 || sortBySecondNumber == 0)) {
 				tempCount++;
-				if (indexFromTemp == -1) {
+				if (indexFromTemp == -1) { // to find first index in current sublist (candidate)
 					indexFromTemp = i - 1;
 				}
 			} else {
@@ -40,8 +41,7 @@ public class ListUtil {
 		}
 
 		if (indexFrom == -1) {
-			indexFrom = 0; // if no such sublist, we return first element of
-							// list
+			indexFrom = 0; // if no such sublist, we return first element of input list
 		}
 
 		return list.subList(indexFrom, indexFrom + count);
---
+++ src/taskLongestSubList/ListUtilTest.java
@@ -0,0 +1,43 @@
+package taskLongestSubList;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
+public class ListUtilTest {
+
+	@Test
+	public void testFindLongestSubList() {
+		List<Pair> list = new ArrayList<>();
+		list.add(new Pair<Integer>(new Integer(1), new Integer(4)));
+		list.add(new Pair<Integer>(new Integer(2), new Integer(5)));
+		list.add(new Pair<Integer>(new Integer(7), new Integer(3)));
+		list.add(new Pair<Integer>(new Integer(4), new Integer(6)));
+		list.add(new Pair<Integer>(new Integer(7), new Integer(7)));
+
+		assertEquals(list.subList(1, 3), ListUtil.findLongestSubList(list));
+	}
+
+	@Test
+	public void testFindLongestSubListForOneElementResult() {
+		List<Pair> list = new ArrayList<>();
+		list.add(new Pair<Integer>(new Integer(1), new Integer(4)));
+		list.add(new Pair<Integer>(new Integer(2), new Integer(5)));
+		list.add(new Pair<Integer>(new Integer(7), new Integer(8)));
+
+		assertEquals(list.subList(0, 1), ListUtil.findLongestSubList(list));
+	}
+
+	@Test
+	public void testFindLongestSubListForListWithNegativeDoubleElements() {
+		List<Pair> list = new ArrayList<>();
+		list.add(new Pair<Double>(new Double(-1.3), new Double(4.8)));
+		list.add(new Pair<Double>(new Double(-3.234), new Double(-5.1)));
+		list.add(new Pair<Double>(new Double(-2.4), new Double(-6.1)));
+		list.add(new Pair<Double>(new Double(1.4), new Double(-8.2)));
+		list.add(new Pair<Double>(new Double(3.44), new Double(0.3)));
+
+		assertEquals(list.subList(1, 4), ListUtil.findLongestSubList(list));
+	}
+}
